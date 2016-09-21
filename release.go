b31c390667b458166a1ca315a87bcdb8303b4826
Chris Hicks <chicks@flow.io>
2016-09-21 12:01:51 -0400

    update s3 bucket for release

--- release.go
+++ release.go
@@ -6,14 +6,14 @@ import (
 
 func main() {
 	executor := executor.Create("track")
-  executor = executor.Add("git checkout master")
-  executor = executor.Add("git pull origin master")
-  executor = executor.Add("dev tag micro")
-  executor = executor.Add("npm install && npm prune")
-  executor = executor.Add("npm run build")
-  executor = executor.Add("sed -i '.bak' 's/APP_VERSION/'$(sem-info tag latest)'/g' dist/index.html")
-	executor = executor.Add("aws s3 sync dist/css s3://io-flow-track-www/css/`sem-info tag latest` --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
-  executor = executor.Add("aws s3 sync dist/js s3://io-flow-track-www/js/`sem-info tag latest` --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
-  executor = executor.Add("aws s3 cp dist/index.html s3://io-flow-track-www --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
+	executor = executor.Add("git checkout master")
+	executor = executor.Add("git pull origin master")
+	executor = executor.Add("dev tag")
+	executor = executor.Add("npm install && npm prune")
+	executor = executor.Add("npm run build")
+	executor = executor.Add("sed -i '.bak' 's/APP_VERSION/'$(sem-info tag latest)'/g' dist/index.html")
+	executor = executor.Add("aws s3 sync dist/css s3://track.flow.io/css/`sem-info tag latest` --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
+	executor = executor.Add("aws s3 sync dist/js s3://track.flow.io/js/`sem-info tag latest` --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
+	executor = executor.Add("aws s3 cp dist/index.html s3://track.flow.io --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers")
 	executor.Run()
 }
