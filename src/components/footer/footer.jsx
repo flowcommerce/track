4da9cf7fa6c271ba1a52bf166054dbd3df96c930
Christian Muñoz <christian.munoz@flow.io>
2016-12-14 15:25:14 -0500

    Render copyright year range
    When current year is greater than 2016 a range year will be displayed
    (e.g. `"2016 - 2017")

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import BemHelper from '../../utilities/bem-helper';
+import formatCopyrightYear from '../../utilities/format-copyright-year';
 
 if (process.browser) {
   require('./styles.css'); // eslint-disable-line global-require
@@ -7,11 +9,12 @@ if (process.browser) {
 
 const bem = new BemHelper('footer');
 
-
 export default function Footer() {
   return (
     <footer className={bem.block()}>
-      <a className={bem.element('link')} href="https://www.flow.io">©{new Date().getFullYear()} Flow Commerce Inc.</a>
+      <a className={bem.element('link')} href="https://www.flow.io">
+        &copy; {formatCopyrightYear('2016')} Flow Commerce Inc.
+      </a>
     </footer>
   );
 }
